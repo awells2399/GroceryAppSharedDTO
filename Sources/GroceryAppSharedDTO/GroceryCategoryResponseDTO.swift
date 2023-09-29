ab4d69f95489d54b29ab647bbbb50aa518bd4ab6
Aaron Wells <alwells239@gmail.com>
2023-09-29 15:41:17 -0400

    public initializer added

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -1,18 +1,18 @@
 //
 //  GroceryCategoryResponseDTO.swift
-//  
+//
 //
 //  Created by Aaron Wells on 9/29/23.
 //
 
 import Foundation
 
-public struct GroceryCategoryResponseDTO: Codable{
+public struct GroceryCategoryResponseDTO: Codable {
     public let id: UUID
     public let title: String
     public let colorCode: String
-    
-    init(id: UUID, title: String, colorCode: String) {
+
+    public init(id: UUID, title: String, colorCode: String) {
         self.id = id
         self.title = title
         self.colorCode = colorCode
